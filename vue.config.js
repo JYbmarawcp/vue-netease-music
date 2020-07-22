1b2f31bd71690a10a51d599d0b712eac792b8a00
JYbmarawcp <fjh2333@gmail.com>
2020-07-22 23:26:44 +0800

    pxtorem

--- vue.config.js
+++ vue.config.js
@@ -18,10 +18,19 @@ module.exports = {
       'vue-router': 'VueRouter',
       vuex: 'Vuex',
       axios: 'axios',
-    }: {},
+    } : {},
   },
   css: {
     loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({ // 把px单位换算成rem单位
+            rootValue: 14, // 换算的基数(设计图750的根字体为75)
+            // selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
+            propList: ['*']
+          })
+        ]
+      },
       sass: {
         prependData: `
           @import "@/style/variables.scss";
