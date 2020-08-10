f9157cf6aad7e00bf4bb86da9f4e17785519842d
JYbmarawcp <fjh2333@gmail.com>
2020-08-10 23:45:33 +0800

    ok

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   outputDir: 'music',
-  publicPath: isProd ? './' : '/',
   configureWebpack: {
     externals: isProd ? {
       vue: 'Vue',
